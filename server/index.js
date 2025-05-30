65013923898e6fc339c62b5a7fc2134fd5e80a0e
Yashwant <prem9810683167@gmail.com>
2025-05-30 15:23:41 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -11,23 +11,7 @@ const app = express();
 
 
 
-const allowedOrigins = [
-  'https://note-app-six-nu.vercel.app',   // your frontend domain
-  // add more if needed, or use '*' carefully
-];
-
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin (like mobile apps, curl)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true, // if you use cookies or auth
-}));
+app.use(cors());
 
 
 
