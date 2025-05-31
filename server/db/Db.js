9bdbecf90911a3e064346c4dc4aa927d964fcf1f
Yashwant <prem9810683167@gmail.com>
2025-05-31 10:26:22 +0530

    final commit

--- server/db/Db.js
+++ server/db/Db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(
-      `${process.env.MONGODB_URL}`); // `${import.meta.env.MONGODB_URL}` this is specific to vite and your local machine.
+    await mongoose.connect(process.env.MONGODB_URL); // `${import.meta.env.MONGODB_URL}` this is specific to vite and your local machine.
     console.log("Connected To MongoDB");
   } catch (err) {
     console.log("Error Connecting To MongoDB", err.message);
